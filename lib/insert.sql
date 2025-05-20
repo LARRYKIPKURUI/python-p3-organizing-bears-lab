0e08193b44eacb4cb34579f37a568834ae8f2257
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-05-20 22:06:07 +0300

    Adding and querying using SQL

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+     ('Mr. Chocolate', 5, 'M', 'brown', 'jovial', 1),
+     ('Rowdy', 3, 'F', 'blonde', 'gentle', 1),
+     ('Tabitha', 4, 'F', 'black', 'rude', 0),
+     ('Sergeant Brown', 7, 'M', 'yellow', 'calm', 1),
+     ('Melissa', 3, 'F', 'blonde', 'jumpy', 1),
+     ('Grinch', 10, 'M', 'brown', 'sweet', 0),
+     ('Wendy', 6, 'M', 'gray', 'sweet', 1),
+     (NULL, 5, 'F', 'white', 'playful', 0);
+
